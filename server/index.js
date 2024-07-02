00aea592a976ed01283f7c74cef14faa99442e12
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-07-01 21:09:03 -0400

    added validation for keys and signature

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+const secp = require('ethereum-cryptography/secp256k1');
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes, hexToBytes, toHex } = require("ethereum-cryptography/utils");
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -7,9 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "12ab18ef5ceac1ee7c254016511dead01b103db1": 100,
+  "bf310d98716609bc21cbcc7c5710e9a72e8a9a00": 50,
+  "24c1ad1a769c59bd33274c1ca9a537282b44a15a": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,16 +23,28 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, recipient, amount, publicKey, signature } = req.body;
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
 
-  if (balances[sender] < amount) {
+  const validKey = validatePublicKeyWithSenderAddress(sender, publicKey);
+  if (!validKey) {
+    res.status(400).send({ message:"Sender wallet address does not match with the provided public key"});
+  }
+
+  const validSignature = validateTrasactionSignature(amount, recipient, publicKey, signature);
+  if (!validSignature) {
+    res.status(400).send({ message:"Unable to validate transaction signature"});
+  }
+
+  const transactionAmount = parseInt(amount);
+
+  if (balances[sender] < transactionAmount) {
     res.status(400).send({ message: "Not enough funds!" });
   } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
+    balances[sender] -= transactionAmount;
+    balances[recipient] += transactionAmount;
     res.send({ balance: balances[sender] });
   }
 });
@@ -41,4 +57,26 @@ function setInitialBalance(address) {
   if (!balances[address]) {
     balances[address] = 0;
   }
+}
+
+function validatePublicKeyWithSenderAddress(sender, publicKey) {
+  const publicKeyBytes = hexToBytes(publicKey);
+  const address = keccak256(publicKeyBytes.slice(1)).slice(-20);
+  return toHex(address) == sender;
+}
+
+function validateTrasactionSignature(amount, recipient, publicKey, signature) {
+  const messageHash = keccak256(
+    utf8ToBytes(
+        JSON.stringify(
+            {
+                amount: amount,
+                recipient: recipient
+            }
+        )
+    )
+  );
+
+  const publicKeyBytes = hexToBytes(publicKey);
+  return secp.verify(hexToBytes(signature), messageHash, publicKeyBytes);
 }
